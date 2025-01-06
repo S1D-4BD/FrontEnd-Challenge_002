0bd57213fdcfc263d45cb3a6c88ecdfa3c491ada
Celina Sid Abdelkader <celina.sid.abdelkader@umontreal.ca>
2025-01-06 15:10:11 -0500

    fixed-redirection

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
+const pageId = document.body.id; //lid de la page ou je suis amendonné
+let rate =""; 
 
-document.addEventListener("DOMContentLoaded", () => {
-
-    const ratingBox = document.querySelectorAll(".rating-box span"); //la ratingBox est l'ensemble des spans dans
-    const rate = document.querySelector(".rating p span");
+if(pageId === "survey-page"){
+    const ratingBox = document.querySelectorAll("form label"); //la ratingBox est la boite des labels dans
+    const button = document.getElementById("btn-submit");
 
     ratingBox.forEach(span =>{ //dans la rating box j'itere chaque span
-
-        span.addEventListener("click",(x) =>{ //des quon entend un clic d'un des span
+        span.addEventListener("click",(x) =>{ //pour chq span des quon entend un clic...
         
-            ratingBox.forEach(span => span.classList.remove("checked"));
-            span.classList.add("checked");
-            rate.textContent = x.target.textContent;
-
+            ratingBox.forEach(span => span.classList.remove("checked")); //on retire le checked de tout les span de rating box
+                span.classList.add("checked"); //mais on le rajoute pour le span ou on a entendu le clic
+                rate=x.target.textContent; //on recup le rate
+                localStorage.setItem("rating-choice", rate);   //on le save
+            });
         });
+    button.addEventListener("click", ()=> {
+        if(rate){
+            window.location.href="survey-answer.html";       
+            }
+        else{
+            window.alert("Notez nous bordel");
+        }
     });
+}
 
+if(pageId === "answer-page") {
+    const answerSpan = document.getElementById("answer");
+    
+    answerSpan.innerHTML =localStorage.getItem("rating-choice"); 
+
+}
+    /*
     const btn = document.querySelector("button");
 
-    btn.addEventListener("click", () =>{
-
+    btn.addEventListener("click", (e) =>{
+        e.preventDefault()
+        
         const a = document.getElementById("rating-view");
         const b = document.getElementById("submit-view");
 
         a.classList.add("hidden");
         b.classList.remove("hidden");
-    });
-
-});
+    });*/
